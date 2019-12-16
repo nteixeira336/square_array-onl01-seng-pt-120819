7144d9bf9a7f3794af372755ed06300c124fda63
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-16 23:31:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def square_array(array)
   # your code here
 result= []
@@ -5,6 +6,7 @@ num=0
 array.each do 
   result << array[num]**2 
   num +=1 
+  binding.pry 
 end 
-result_array 
+result
 end
